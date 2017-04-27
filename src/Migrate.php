ce4cb9931478e4d3f3a4fec49a8a671a3093e36b
Markus Perl <markus@www-factory.de>
2017-04-27 18:39:31 +0200

    Bugfix migration

--- src/Migrate.php
+++ src/Migrate.php
@@ -15,7 +15,7 @@ class Migrate extends \ZFMigrate\Model\AbstractMigration
                                 `version` SMALLINT(5) UNSIGNED NOT NULL
                                 ) ENGINE=InnoDB DEFAULT CHARSET=utf8; ALTER TABLE `migrate`
                               ADD PRIMARY KEY (`module`);
-                              INSERT INTO `migrate` (`module`, `version`) VALUES (\'migrate\', \'1\')
+                              INSERT INTO `migrate` (`module`, `version`) VALUES (\'ZFmigrate\', \'1\')
                               ', Adapter::QUERY_MODE_EXECUTE);
         });
     }
